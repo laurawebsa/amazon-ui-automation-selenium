863b068b07d8ee62f295f89e536ad59536ec91a0
“Laura <lauraguep@gmail.com>
2025-12-24 13:00:52 -0500

    Updates

--- src/test/java/TestNG/DemoClass_3.java
+++ src/test/java/TestNG/DemoClass_3.java
@@ -5,6 +5,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class DemoClass_3 {
@@ -12,10 +14,11 @@ public class DemoClass_3 {
 public void bfClass(){
     System.out.println("Before executing any method in the class");
 }
-
+    @Parameters({"URL"})
     @Test //Anotación de librería TestNG
-    public void webLoginCar(){
+    public void webLoginCar(String urlName){
         System.out.println("Web Login Car");
+        System.out.println(urlName);
     }
 
     @BeforeMethod
@@ -46,15 +49,40 @@ public void bfClass(){
     public void mobileSignInCar(){
         System.out.println("Mobile Sign in Car");
     }
-    @Test
-    public void mobileSingOutCar(){
+    @Test(dataProvider = "getData")
+    public void mobileSingOutCar(String username, String password){
         System.out.println("Mobile Sign out Car");
+        System.out.println(username);
+        System.out.println(password);
     }
     //este bloque depende de webLoginCar método, entonces se ejecuta primero este método y luego
     //si el método APILoginCar
     @Test(dependsOnMethods = {"webLoginCar"})
     public void APILoginCar(){
         System.out.println("API Login Car");
+    }
+
+    @DataProvider
+    public Object[][] getData(){
+    //1st combination - username password - good credit history - row
+    //2nd - username password - no credit history
+    //3rd - fraudulent credit history
+        Object[][] data = new Object[3][2];
+        //1st set
+        data[0][0] = "firstusername";
+        data[0][1] = "firstpassword";
+        //columns in the row are nothing but values for that particular combination(row)
+
+        //2nd set
+        data[1][0] = "secondusername";
+        data[1][1] = "secondpassword";
+
+        //3rd set
+        data[2][0] ="thirdusername";
+        data[2][1] ="thirdpassword";
+        return data;
+
+
     }
 
 }
--- src/test/java/TestNG/DemoClass_4.java
+++ src/test/java/TestNG/DemoClass_4.java
@@ -1,12 +1,15 @@
 package TestNG;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class DemoClass_4 {
 
+    @Parameters({"URL"})
     @Test //Anotación de librería TestNG
-    public void webHomeCar(){
+    public void webHomeCar(String uName){
         System.out.println("Web Home Car");
+        System.out.println(uName);
     }
 
     @Test(groups = {"Smoke"})
