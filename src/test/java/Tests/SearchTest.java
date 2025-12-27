7e1e02119845a730cf58745ff852c1a5f5009f1b
“Laura <lauraguep@gmail.com>
2025-12-27 18:55:02 -0500

    Updates

--- src/test/java/Tests/SearchTest.java
+++ src/test/java/Tests/SearchTest.java
@@ -1,4 +1,23 @@
 package Tests;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class SearchTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void searchProduct(){
+        driver.get("https://www.amazon.com/");
+    }
 }
---
+++ src/test/java/pages/SerchTest.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+public class SerchTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+
+    }
+
+
+}
