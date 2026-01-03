5f60c8b8dca4d7d47809725ff180cc25ad867a37
“Laura <lauraguep@gmail.com>
2026-01-02 19:32:50 -0500

    Updates

--- src/test/java/Tests/SearchTest.java
+++ src/test/java/Tests/SearchTest.java
@@ -1,7 +1,10 @@
 package Tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -18,6 +21,21 @@ public class SearchTest {
 
     @Test
     public void searchProduct(){
+
         driver.get("https://www.amazon.com/");
+        driver.findElement(By.id("twotabsearchtextbox"));
+        driver.findElement(By.id("nav-search-submit-button")).click();
+
+        String title = driver.getTitle();
+
+        Assert.assertTrue("El titulo no contiene la palabra buscada",
+                title.toLowerCase().contains("laptop"));
+
     }
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+
 }
